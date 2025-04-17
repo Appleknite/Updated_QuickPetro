b0a4c53e84ac1277e00ff5901f57837dfc333bff
DEV GANDHI <157933549+Appleknite@users.noreply.github.com>
2025-04-17 16:06:12 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import AppRoutes from './routes/index.tsx';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col bg-white">
         <Header />
         <div className="flex-grow">
