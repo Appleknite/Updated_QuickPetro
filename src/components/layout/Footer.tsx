2a84c7f6543bea73f869d12e1e2c1078cd174d10
DEV GANDHI <157933549+Appleknite@users.noreply.github.com>
2025-04-27 20:41:22 -0700

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -94,7 +94,7 @@ const Footer = () => (
       </div>
 
       <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-        <p>© 2024 QuickPetro. All rights reserved.</p>
+        <p>© 2025 QuickPetro. All rights reserved.</p>
       </div>
     </div>
   </footer>
