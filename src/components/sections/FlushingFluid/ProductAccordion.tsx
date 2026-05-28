71bd81123c87fcfffc58a40498c455ab649f36cf
DEV GANDHI <157933549+Appleknite@users.noreply.github.com>
2026-05-28 11:46:02 +0530

    Update ProductAccordion.tsx

--- src/components/sections/FlushingFluid/ProductAccordion.tsx
+++ src/components/sections/FlushingFluid/ProductAccordion.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const ProductAccordion = () => {
   return (
     <div>
-      <h2>Product Accordion</h2>
-      <p>This component is now alive. Congratulations.</p>
+
     </div>
   );
 };
