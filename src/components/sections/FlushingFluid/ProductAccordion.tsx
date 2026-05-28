09b7b1e99aabb0c4c89fd046f31c609458dee586
DEV GANDHI <157933549+Appleknite@users.noreply.github.com>
2026-05-28 11:44:51 +0530

    Create ProductAccordion component
    Added a new ProductAccordion component with a header and message.

--- src/components/sections/FlushingFluid/ProductAccordion.tsx
+++ src/components/sections/FlushingFluid/ProductAccordion.tsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+const ProductAccordion = () => {
+  return (
+    <div>
+      <h2>Product Accordion</h2>
+      <p>This component is now alive. Congratulations.</p>
+    </div>
+  );
+};
+
+export default ProductAccordion;
